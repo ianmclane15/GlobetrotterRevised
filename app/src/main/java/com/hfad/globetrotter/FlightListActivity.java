77e13388886c891aa52999827bde124ea65c7247
ianmclane15 <37561756+ianmclane15@users.noreply.github.com>
2019-05-29 22:51:55 -0700

    Initial commit

---
+++ app/src/main/java/com/hfad/globetrotter/FlightListActivity.java
@@ -0,0 +1,48 @@
+package com.hfad.globetrotter;
+
+import android.content.Intent;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FlightListActivity extends AppCompatActivity {
+
+    private ListView flights;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight_list);
+
+        wireWidgets();
+
+        populateListView();
+
+        registerForContextMenu(flights);
+
+    }
+
+    private void populateListView() {
+        Carrier[] carrierArray;
+        carrierArray = (Carrier[])getIntent().getParcelableArrayExtra("carriers");
+        List<Carrier> carriers = Arrays.asList(carrierArray);
+        ArrayAdapter<Carrier> arrayAdapter = new ArrayAdapter<Carrier>(this, android.R.layout.simple_list_item_1, carriers);
+        flights.setAdapter(arrayAdapter);
+    }
+
+    private void wireWidgets() {
+        flights = findViewById(R.id.listView_flightListActivity_list);
+    }
+}
